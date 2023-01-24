c85b1b3bd5b41aeeb95f2535cfbda20cceb70b5d
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-24 15:59:46 +0100

    feat(env/prod): Clone dev environment into production

---
+++ env/prod/main.tf
@@ -0,0 +1,20 @@
+locals {
+  prefix  = "prod"
+  zone_id = "f2c00168a7ecd694bb1ba017b332c019"
+}
+
+resource "aws_key_pair" "default" {
+  key_name   = "${local.prefix}-default"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCslNKgLyoOrGDerz9pA4a4Mc+EquVzX52AkJZz+ecFCYZ4XQjcg2BK1P9xYfWzzl33fHow6pV/C6QC3Fgjw7txUeH7iQ5FjRVIlxiltfYJH4RvvtXcjqjk8uVDhEcw7bINVKVIS856Qn9jPwnHIhJtRJe9emE7YsJRmNSOtggYk/MaV2Ayx+9mcYnA/9SBy45FPHjMlxntoOkKqBThWE7Tjym44UNf44G8fd+kmNYzGw9T5IKpH1E1wMR+32QJBobX6d7k39jJe8lgHdsUYMbeJOFPKgbWlnx9VbkZh+seMSjhroTgniHjUl8wBFgw0YnhJ/90MgJJL4BToxu9PVnH"
+}
+
+module "vm" {
+  source   = "../../modules/ec2"
+  zone_id  = local.zone_id
+  name     = "prod-hello"
+  key_name = aws_key_pair.default.key_name
+}
+
+output "domain" {
+  value = module.vm.domain
+}
