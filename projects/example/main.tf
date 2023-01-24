b65038d6bd040d2f3b36a22e9f0aeb4cd4d1830a
Ondrej Sika <ondrej@ondrejsika.com>
2023-01-24 16:04:58 +0100

    refactor: Move project to own module

---
+++ projects/example/main.tf
@@ -0,0 +1,27 @@
+variable "prefix" {
+  type = string
+}
+variable "zone_id" {
+  type = string
+}
+
+locals {
+  prefix  = var.prefix
+  zone_id = var.zone_id
+}
+
+resource "aws_key_pair" "default" {
+  key_name   = "${local.prefix}-default"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCslNKgLyoOrGDerz9pA4a4Mc+EquVzX52AkJZz+ecFCYZ4XQjcg2BK1P9xYfWzzl33fHow6pV/C6QC3Fgjw7txUeH7iQ5FjRVIlxiltfYJH4RvvtXcjqjk8uVDhEcw7bINVKVIS856Qn9jPwnHIhJtRJe9emE7YsJRmNSOtggYk/MaV2Ayx+9mcYnA/9SBy45FPHjMlxntoOkKqBThWE7Tjym44UNf44G8fd+kmNYzGw9T5IKpH1E1wMR+32QJBobX6d7k39jJe8lgHdsUYMbeJOFPKgbWlnx9VbkZh+seMSjhroTgniHjUl8wBFgw0YnhJ/90MgJJL4BToxu9PVnH"
+}
+
+module "vm" {
+  source   = "../../modules/ec2"
+  zone_id  = local.zone_id
+  name     = "${local.prefix}-hello"
+  key_name = aws_key_pair.default.key_name
+}
+
+output "domain" {
+  value = module.vm.domain
+}
